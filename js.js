5d5f80806525a141943efb70bb9d60d5a3531372
goblocksquad <bonzardaily.origin@gmail.com>
2023-05-15 20:07:37 +0700

    komit pertama

---
+++ js.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("popup").style.display = "block";
+  });
+  
+  document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+  });
+
+  
