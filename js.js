852e574072128be818fd69ad05d7baf3fbf899c7
goblocksquad <bonzardaily.origin@gmail.com>
2023-05-16 14:51:31 +0700

    Pembaharuan menyeluruh versi 1

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("popup").style.display = "block";
-  });
-  
-  document.getElementById("closePopup").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "none";
-  });
+// POPUP
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("popup").style.display = "block";
+});
 
-  
+document.getElementById("closePopup").addEventListener("click", function () {
+  document.getElementById("popup").style.display = "none";
+});
+
+// SPINNER
+window.addEventListener("load", function () {
+  document.body.classList.add("loaded");
+});
