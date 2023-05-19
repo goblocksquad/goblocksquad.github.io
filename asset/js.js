d530ec2395bcf1f443a106f8863e6ab0df21281e
goblocksquad <bonzardaily.origin@gmail.com>
2023-05-19 23:35:17 +0700

    Perbaikan toast

--- asset/js.js
+++ asset/js.js
@@ -11,3 +11,14 @@ document.getElementById("closePopup").addEventListener("click", function () {
 window.addEventListener("load", function () {
   document.body.classList.add("loaded");
 });
+
+function showToast() {
+  var toast = document.getElementById('toast');
+  toast.classList.remove('hide');
+  toast.classList.add('show');
+
+  setTimeout(function() {
+    toast.classList.remove('show');
+    toast.classList.add('hide');
+  }, 7000); // Durasi tampilan toast dalam milidetik (misalnya, 3000ms untuk 3 detik)
+}
