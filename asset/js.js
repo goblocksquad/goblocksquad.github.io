540ffa56c53c6a120935625050d2280aae6c9cd2
Your Name <bonzardaily.origin@gmail.com>
2023-12-12 15:20:03 +0700

    Update Event Theme Natal

--- asset/js.js
+++ asset/js.js
@@ -25,3 +25,20 @@ function showToast() {
     toast.classList.add("hide");
   }, 7000); // Durasi tampilan toast dalam milidetik (misalnya, 3000ms untuk 3 detik)
 }
+
+// SALJU
+  document.addEventListener("DOMContentLoaded", function() {
+const container = document.getElementById("snowfall-container");
+
+for (let i = 0; i < 100; i++) {
+  createSnowflake();
+}
+
+function createSnowflake() {
+  const snowflake = document.createElement("div");
+  snowflake.classList.add("snowflake");
+  snowflake.style.left = `${Math.random() * 100}vw`; // vw untuk posisi horizontal di seluruh lebar layar
+  snowflake.style.animationDuration = `${Math.random() * 6 + 5}s`; // Durasi animasi acak antara 2 hingga 5 detik
+  container.appendChild(snowflake);
+}
+});
